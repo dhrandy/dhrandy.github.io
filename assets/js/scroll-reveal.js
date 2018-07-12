bad71c1a541536a3f285f65db2ae9576841fcc27
dhrandy <dhrandy@gmail.com>
2018-07-11 20:31:10 -0400

    update

---
+++ assets/js/scroll-reveal.js
@@ -0,0 +1,55 @@
+window.sr = ScrollReveal();
+sr.reveal('.navbar', {
+    duration: 2000,
+    origin: 'bottom'
+})
+sr.reveal('.facebookIcon', {
+    duration: 2000,
+    origin: 'top',
+    distance: '300px'
+})
+sr.reveal('.twitterIcon', {
+    duration: 4000,
+    origin: 'top',
+    distance: '300px'
+})
+sr.reveal('.linkedinIcon', {
+    duration: 6000,
+    origin: 'top',
+    distance: '300px'
+})
+sr.reveal('.githubIcon', {
+    duration: 7000,
+    origin: 'top',
+    distance: '300px'
+})
+sr.reveal('#meImage', {
+    duration: 2000,
+    origin: 'left',
+    distance: '200px'
+})
+sr.reveal('.fa-code', {
+    duration: 2000,
+    origin: 'left',
+    distance: '300px'
+})
+sr.reveal('.fa-css3-alt', {
+    duration: 2000,
+    origin: 'top',
+    distance: '300px'
+})
+sr.reveal('.fa-js', {
+    duration: 2000,
+    origin: 'bottom',
+    distance: '300px'
+})
+sr.reveal('.fa-database', {
+    duration: 2000,
+    origin: 'right',
+    distance: '300px'
+})
+sr.reveal('.fa-envelope', {
+    duration: 2000,
+    origin: 'left',
+    distance: '200px'
+})
